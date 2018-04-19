949a2ce8abad4ef3ede188661cab8396673c65d3
Topolic <jovantopolic@gmail.com>
2018-04-19 15:32:50 +0200

    Ispravljena greska na frontendu

--- Project1/src/main/resources/static/scripts/userpage.js
+++ Project1/src/main/resources/static/scripts/userpage.js
@@ -31,6 +31,58 @@ $(document).on('submit','.editform', function(e) {
 	}
 });
 
+function generateNavbar2(){
+	$.ajax({
+		 url: islogged_url,
+		 method: "GET",
+		 success: function(data){
+			 var user = data;
+			 if(user.email!=null){
+				 $(".navitems").empty();
+				 $(".navitems").append(`<li class="nav-item active">
+	                <a class="nav-link" href="#">Home
+	                  <span class="sr-only">(current)</span>
+	                </a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link" href="fanzone.html">FanZone</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link" href="#">About</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link" href="#">Services</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link" href="userpage.html">User `+user.name+` signed in</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link" href="index.html" onclick="logout()">Sign out</a>
+	              </li>`);
+			 }else{
+			    	$(".navitems").empty();
+			    	$(".navitems").append(`<li class="nav-item active">
+			                <a class="nav-link" href="#">Home
+			                  <span class="sr-only">(current)</span>
+			                </a>
+			              </li>
+			              <li class="nav-item">
+			                <a class="nav-link" href="#">About</a>
+			              </li>
+			              <li class="nav-item">
+			                <a class="nav-link" href="register.html">Register</a>
+			              </li>
+			              <li class="nav-item">
+			                <a class="nav-link" href="signin.html">Sign in</a>
+			              </li>`);
+			 }
+	        
+	    },
+	    error:function(data){
+	    	
+	    }
+	 });
+}
 
 function formToJSON() {
 	return JSON.stringify({
